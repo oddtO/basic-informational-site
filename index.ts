3a62310de13c062b0c15699e3c06734fdb1ed627
oddtO <121516833+oddtO@users.noreply.github.com>
2024-08-09 01:12:42 +0300

    finish task

---
+++ index.ts
@@ -0,0 +1,21 @@
+import http from "node:http";
+import fs from "node:fs/promises";
+
+const PAGES = ["./index", "./about", "./contact-me"];
+
+http
+  .createServer(async (req, res) => {
+    let fileName = "." + (req.url === "/" ? "/index" : req.url);
+
+    if (PAGES.includes(fileName)) fileName += ".html";
+    let data = null;
+    try {
+      data = await fs.readFile(fileName);
+    } catch {
+      data = await fs.readFile("./404.html");
+      res.statusCode = 404;
+    }
+    res.write(data);
+    res.end();
+  })
+  .listen(8000);
